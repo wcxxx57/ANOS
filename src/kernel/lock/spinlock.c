5bc86afa5ee03644847d28807ff12a5665d79552
dxy831 <2909273804@qq.com>
2025-09-24 01:37:18 +0800

    初步完成了spinlock.c

--- src/kernel/lock/spinlock.c
+++ src/kernel/lock/spinlock.c
@@ -35,23 +35,54 @@ void pop_off(void)
 // 自选锁初始化
 void spinlock_init(spinlock_t *lk, char *name)
 {
-
+    // 初始状态：未上锁（无cpu持有该锁）
+    lk->locked = 0;
+    lk->name = name;
+    lk->cpuid = -1;
 }
 
 // 是否持有自旋锁
 bool spinlock_holding(spinlock_t *lk)
 {
-
+    // 检查当前的cpu是否持有该锁
+    return lk->locked && lk->cpuid == mycpuid();
 }
 
 // 获取自选锁
+// 原地循环，直到能够获取锁
 void spinlock_acquire(spinlock_t *lk)
 {
+    // 关中断
+    push_off();
 
+    // 确保当前cpu没有已经持有该锁
+    assert(!spinlock_holding(lk),"spinlock_acquire: lock is already held by this cpu\n");
+
+    // 自旋等待，直到成功获取锁
+    while (__sync_lock_test_and_set(&lk->locked,1)!=0) // 原子操作：尝试获取锁
+        ;
+
+    // 防止指令的乱序执行导致锁机制失效
+    __sync_synchronize();
+    // 记录该锁的cpu信息
+
+    lk->cpuid = mycpuid();
 }
 
 // 释放自旋锁
 void spinlock_release(spinlock_t *lk)
 {
+    // 确保当前cpu持有锁
+    assert(spinlock_holding(lk),"spinlock_release: lock is not held by this cpu\n");
 
+    // 清除该锁的cpu信息
+    lk->cpuid = -1;
+
+    __sync_synchronize();
+
+    // 释放锁
+    __sync_lock_release(&lk->locked); // 原子操作
+
+    // 开中断
+    pop_off();
 }
