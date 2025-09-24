60a726f88cd6e05b8d979945e0f5d3d45ccf725e
wcxx57 <10242150443@stu.ecnu.edu.cn>
2025-09-24 23:50:39 +0800

    在print.c中使用了自旋锁，实现了顺序打印

---
+++ target/kernel/lock/spinlock.d
@@ -0,0 +1,7 @@
+target/kernel/lock/spinlock.o: src/kernel/lock/spinlock.c \
+ src/kernel/lock/mod.h src/kernel/lock/type.h \
+ src/kernel/lock/../arch/type.h src/kernel/lock/method.h \
+ src/kernel/lock/../arch/mod.h src/kernel/lock/../arch/method.h \
+ src/kernel/lock/../lib/mod.h src/kernel/lock/../lib/type.h \
+ /usr/lib/gcc/riscv64-unknown-elf/10.2.0/include/stdarg.h \
+ src/kernel/lock/../lib/method.h
