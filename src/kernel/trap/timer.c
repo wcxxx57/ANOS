46ecb693ab92837e1efe8e5a4f321e626ee1b4d2
wcxx57 <10242150443@stu.ecnu.edu.cn>
2025-10-13 13:39:38 +0800

    add source code

---
+++ src/kernel/trap/timer.c
@@ -0,0 +1,61 @@
+#include "mod.h"
+
+/*-------------------- 工作在M-mode --------------------*/
+
+// in trap.S M-mode时钟中断处理流程()
+extern void timer_vector();
+
+// 每个CPU在时钟中断中需要的临时空间
+static uint64 mscratch[NCPU][5];
+
+// 时钟初始化
+void timer_init()
+{
+    // 获取当前cpuid
+    int hartid = r_tp();
+
+    // 设置初始值 cmp_time = cur_time + time_interval
+    *(uint64*)CLINT_MTIMECMP(hartid) = *(uint64*)CLINT_MTIME + INTERVAL;
+
+    // cur_mscratch 指向当前CPU的msrcatch数组
+    uint64* cur_mscratch = mscratch[hartid];
+
+    // cur_mscratch[1] [2] [3]先空着, 在trap.S里使用
+    cur_mscratch[3] = CLINT_MTIMECMP(hartid); // cmp_time
+    cur_mscratch[4] = INTERVAL;               // interval
+
+    // 存放到临时寄存器, 便于与trap.S中的timer_vector协作
+    w_mscratch((uint64)cur_mscratch);
+
+    // 设置 M-mode 中断处理函数
+    w_mtvec((uint64)timer_vector);
+
+    // 打开 M-mode 中断总开关
+    w_mstatus(r_mstatus() | MSTATUS_MIE);
+
+    // 打开 M-mode 时钟中断分开关
+    w_mie(r_mie() | MIE_MTIE);
+}
+
+/*--------------------- 工作在S-mode --------------------*/
+
+// 全局系统时钟
+static timer_t sys_timer;
+
+// 时钟创建
+void timer_create()
+{
+
+}
+
+// 时钟更新
+void timer_update()
+{
+
+}
+
+// 获取滴答数量 (不把sys_timer暴露出去, 只提供安全的访问接口)
+uint64 timer_get_ticks()
+{
+
+}
--- src/kernel/trap/type.h
+++ src/kernel/trap/type.h
@@ -1,9 +1,11 @@
 #pragma once
-#include "../arch/type.h"
+#include "../lib/type.h"
+#include "../lock/type.h"
 
-// platform-level interrupt controller(PLIC)
+// platform-level interrupt controller (PLIC)
+// 接收外部中断源: 主要是外设中断
 #define PLIC_BASE 0x0c000000ul
-#define PLIC_PRIORITY(id) (PLIC_BASE + (id) * 4)
+#define PLIC_PRIORITY(hart) (PLIC_BASE + (hart) * 4)
 #define PLIC_PENDING (PLIC_BASE + 0x1000)
 #define PLIC_MENABLE(hart) (PLIC_BASE + 0x2000 + (hart) * 0x100)
 #define PLIC_SENABLE(hart) (PLIC_BASE + 0x2080 + (hart) * 0x100)
@@ -12,8 +14,18 @@
 #define PLIC_MCLAIM(hart) (PLIC_BASE + 0x200004 + (hart) * 0x2000)
 #define PLIC_SCLAIM(hart) (PLIC_BASE + 0x201004 + (hart) * 0x2000)
 
-// core local interruptor(CLINT)
+// core-local interruptor (CLINT)
+// 接收本地中断源: 包括软件中断和时钟中断
 #define CLINT_BASE 0x2000000ul
-#define CLINT_MSIP(hartid) (CLINT_BASE + 4 * (hartid))
-#define CLINT_MTIMECMP(hartid) (CLINT_BASE + 0x4000 + 8 * (hartid))
+#define CLINT_MSIP(hart) (CLINT_BASE + 4 * (hart))
+#define CLINT_MTIMECMP(hart) (CLINT_BASE + 0x4000 + 8 * (hart))
 #define CLINT_MTIME (CLINT_BASE + 0xBFF8)
+
+// 每隔INTERVAL个cycle发生一次时钟中断 (1e6个cycle大约为0.1s)
+#define INTERVAL 1000000
+
+// 计时器
+typedef struct timer {
+    uint64 ticks;  /* 每发生一次时钟中断ticks++ */
+    spinlock_t lk; /* 保证ticks的更新是原子的 */
+} timer_t;
