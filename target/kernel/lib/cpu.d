f98ef8452d513580d7ebf794c2a0fce802644270
dxy831 <2909273804@qq.com>
2025-10-15 17:25:28 +0800

    完成main.c和timer.c

---
+++ target/kernel/lib/cpu.d
@@ -0,0 +1,7 @@
+target/kernel/lib/cpu.o: src/kernel/lib/cpu.c src/kernel/lib/mod.h \
+ src/kernel/lib/type.h src/kernel/lib/../arch/type.h \
+ /usr/lib/gcc-cross/riscv64-linux-gnu/11/include/stdarg.h \
+ src/kernel/lib/method.h src/kernel/lib/../lock/mod.h \
+ src/kernel/lib/../lock/type.h src/kernel/lib/../lock/method.h \
+ src/kernel/lib/../lock/../arch/mod.h \
+ src/kernel/lib/../lock/../arch/method.h
