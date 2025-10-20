242e3476b94e65198de70a31bd8bc8960de4a20a
wcxx57 <10242150443@stu.ecnu.edu.cn>
2025-10-20 20:07:24 +0800

    通过了串口输入测试

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -7,58 +7,68 @@ volatile static int started = 0;
 
 int main()
 {    
+    // 初始化UART串口
+    uart_init();
     // 初始化 S-mode 的中断/异常处理
     trap_kernel_init();
     trap_kernel_inithart();
+    
+    //---[DEBUG]查看相关硬件寄存器配置是否正确---
+    // printf("=== DEBUG START ===\n");
+    // printf("STVEC: %p\n", r_stvec());
+    // printf("SSTATUS: %p\n", r_sstatus() & SSTATUS_SIE ? "SIE=ON" : "SIE=OFF");
+    
+    // uint32 ier = ReadReg(IER);
+    // printf("UART IER = 0x%x\n", ier);
 
-    // 创建系统时钟
-    timer_create();
+    // uint32 plic_en = *(volatile uint32*)PLIC_SENABLE(0);
+    // printf("PLIC enable = 0x%x\n", plic_en);
+    
+    // extern char kernel_vector[];
+    // printf("kernel_vector addr: %p\n", kernel_vector);
 
-    plic_init(); // 初始化 PLIC
-    uart_init();
-    printf("All subsystems initialized\n");
 
-    // 打开 S-mode 中断
-    intr_on();
-
+    // 串口输入测试
     int cpuid = mycpuid();
     if (cpuid == 0) {
-        print_init();
         printf("CPU %d is booting!\n", cpuid);
-        __sync_synchronize();
+        __sync_synchronize(); // 防止指令重排
         started = 1;
+
+        while (1) {
+            asm volatile("wfi");  // 等待中断
+        }
     } else {
         while (started == 0);
         __sync_synchronize();
         printf("CPU %d is booting!\n", cpuid);
-    }
 
+        while (1) {
+            asm volatile("wfi");  // 等待中断
+        }
+    }
+   
+
+    
     // int cpuid = mycpuid();
     // if (cpuid == 0) {
+    //     print_init();
     //     printf("CPU %d is booting!\n", cpuid);
     //     __sync_synchronize();
     //     started = 1;
-
-    //     while (1) {
-    //         asm volatile("wfi");  // 等待中断
-    //     }
     // } else {
     //     while (started == 0);
     //     __sync_synchronize();
     //     printf("CPU %d is booting!\n", cpuid);
+    // }
 
-    //     while (1) {
-    //         asm volatile("wfi");  // 等待中断
+    // //  滴答测试
+    // uint64 last = (uint64)-1;
+    // while (1) {
+    //     uint64 t = timer_get_ticks();
+    //     if (t != last) {
+    //         last = t;
+    //         printf("cpu %d:di da\n", cpuid);
     //     }
     // }
-   
-    // // 滴答测试
-    uint64 last = (uint64)-1;
-    while (1) {
-        uint64 t = timer_get_ticks();
-        if (t != last) {
-            last = t;
-            printf("cpu %d:di da\n", cpuid);
-        }
-    }
 }
